0e5edb46c684dd1609aa684682f9b335cd63f262
Dyegho <dyeghocunha@gmail.com>
2023-04-28 19:59:29 -0300

    Finalizando botao Foguete

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -1,14 +1,11 @@
 
+import BotaoFoguete from '../../Componentes/BotaoFoguete'
 import styles from './PaginaInicial.module.scss'
-import BotaoCircular from '../../Componentes/BotaoCircular'
-
 
 
 export default function PaginaInicial() {
   return (
-    <div className={styles.botao}>
-     <BotaoCircular />
-    </div>
+   <BotaoFoguete/>
     
   )
 }
