116d0a44aa09f8a9ed14d79c313e7df9689893f9
Dyegho <dyeghocunha@gmail.com>
2023-05-01 09:57:50 -0300

    Rodape Iniciando

--- src/Componentes/Post/index.jsx
+++ src/Componentes/Post/index.jsx
@@ -1,37 +1,36 @@
 import BotaoMenu from 'Componentes/Botoes/BotaoMenu'
 import styles from './Post.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookOpenReader} from '@fortawesome/free-solid-svg-icons';
+import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 
-export default function Post({ post }) {  
+export default function Post({ post }) {
 
-const icone = <FontAwesomeIcon icon={faBookOpenReader} />
+
   return (
     <div className={`${styles.post} ${styles.loader}`}>
 
       <div className={styles.rocket}>
 
-        <div className={styles.containerPost}>
+        <section className={styles.containerPost}>
 
           <img
             className={styles.capa}
             src={`/assets/posts/${post.id}/capa.png`}
             alt="Imagem de capa do Post" />
 
+          <div className={styles.faixa}>
+            <h2 className={styles.titulo}>{post.titulo}</h2>
+            <button className={styles.botaoLer}>Ler</button>
+          </div>
 
-          <button><BotaoMenu icone={icone} /></button>
-        </div>
+
+        </section>
 
         <div className={`${styles.faRocket} `}>
-
           <div className={styles.hover}></div>
-
           <span><i></i></span>
-
         </div>
-
-
 
       </div>
 
